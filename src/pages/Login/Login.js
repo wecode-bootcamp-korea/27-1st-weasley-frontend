8299e76ea599cff42304ccb4f6cfd9a9d27d9396
yuryang Kim <flowratekim@gmail.com>
2021-12-01 20:09:23 +0900

    Modify : sider 이슈 해결

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,29 +1,31 @@
-import React, { useState } from 'react';
-import { useNavigate, Link } from 'react-router-dom';
+import React from 'react';
+import { Link } from 'react-router-dom';
 import '../Login/Login.scss';
 
 function Login() {
-  // useNavigate 써서 통과하면 signup
-  // const navigate = useNavigate();
-  // const [loginValue, setLoginValue] = useState('');
-  // const [emailValue, setEmailValue] = useState([]);
-
   return (
-    <div className="Login">
+    <div className="login">
       <div className="logo">
         <Link to="/" />
       </div>
+
       <div className="loginSection">
-        <p>
-          <b>로그인 및 회원가입</b>을 시작합니다.
-        </p>
-        <div className="loginEmail">
-          <form name="loginForm" action="login.js" method="post">
-            <label for="email">
-              {/* <input value={loginValue} placeholder="이메일" /> */}
-            </label>
-          </form>
-        </div>
+        <h1 className="loginH1">
+          로그인 및 회원가입
+          <span className="loginTitle">을 시작합니다.</span>
+        </h1>
+
+        <form name="loginForm" action="login.js" method="post">
+          <label for="email">
+            <input
+              value=""
+              type="text"
+              id="email"
+              name="email"
+              placeholder="이메일"
+            />
+          </label>
+        </form>
         <p>
           <b>위즐리 컴퍼니 통합 회원으로 진행됩니다.</b>
         </p>
