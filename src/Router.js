3d5be0a24364943e1929fc466ca9a8ef7d7dfff6
JooyoungHwnag <71796608+JooyoungHwnag@users.noreply.github.com>
2021-12-01 22:37:55 +0900

    Add: 결제페이지 레이아웃#1

--- src/Router.js
+++ src/Router.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ProductList from './pages/ProductList/ProductList';
-import ProductDetail from './pages/ProductDetail/ProductDetail';
+
 import Login from './pages/Login/Login';
+import Payment from './pages/payment/Payment';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/productlist" element={<ProductList />} />
-        <Route path="/productdetail" element={<ProductDetail />} />
+
         <Route path="/login" element={<Login />} />
+        <Route path="/payment" element={<Payment />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/ProductDetail/ProductDetail.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const ProductDetail = () => {
-  return <div>프로덕트디테일페이지입니다.</div>;
-};
-
-export default ProductDetail;
