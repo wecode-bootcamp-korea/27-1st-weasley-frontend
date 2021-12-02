b4d959b2cb52d95bc98fa0af9c5a5417a739f615
seongjae0325 <90776802+seongjae0325@users.noreply.github.com>
2021-12-02 17:00:35 +0900

    [27기 황성재]장바구니 페이지 구현중

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
+import EmptyCart from './EmptyCart';
 import '../Cart/Cart.scss';
+
 //http://10.58.0.114:8000/shops/carts
 function Cart() {
   useEffect(() => {
@@ -14,120 +16,141 @@ function Cart() {
       .then(data => {
         //data.RESULT.length
         if (data.length === 0) {
-          alert('장바구니가 비어있습니다. 홈으로 이동합니다.');
-          window.location.replace('http://localhost:3005/');
+          alert('장바구니가 비었습니다.');
+          setEmpty(true);
         } else {
           setCart(data);
         }
       });
   }, []);
 
-  function handlePlus() {
-    fetch('장바구니 api', {
-      method: 'patch',
-      body: JSON.stringify({
-        id: 'id',
-        // amount: amount + 1,
-      }),
-    })
-      .then(res => res.json('/'))
-      .then(data => {
-        setCart(data);
-      })
-      .catch(error => alert(error));
-  }
-
-  function handleMinus() {
-    fetch('장바구니 api', {
-      method: 'post',
-      body: JSON.stringify({
-        product_id: 'id',
-        value: 'minus',
-      }),
-    })
-      .then(res => res.json())
-      .then(data => {
-        setCart(data);
-      })
-      .catch(error => alert(error));
-  }
-
-  function handleDelete() {
-    fetch('장바구니 api', {
-      method: 'delete',
-      body: JSON.stringify({
-        product_id: 'id',
-      }),
-    })
-      .then(res => res.json())
-      .then(data => {
-        setCart(data);
-      })
-      .catch(error => alert(error));
-  }
-
   const [cart, setCart] = useState([]);
-  const [count, setCount] = useState();
 
+  const [empty, setEmpty] = useState(false);
+
+  console.log(cart);
   return (
     <>
       <nav>네브바</nav>
+      {empty ? (
+        <EmptyCart />
+      ) : (
+        <main className="cartMain">
+          <div className="title">장바구니</div>
 
-      <main className="cartMain">
-        <div className="title">장바구니</div>
-
-        {cart.map(function (list, i) {
-          return (
-            <div className="list">
-              <div className="listLeft">
-                <img src={list.thumb} />
-                <div className="listArticle">
-                  <h3>{list.category_name}</h3>
-                  <span>
-                    {list.ml_volume}ml / {list.tags.join('')}용
-                  </span>
-                  <p>{list.price.toLocaleString()}원</p>
-                </div>
-              </div>
-
-              <div className="listRight">
-                <div className="cancel">
-                  <button onClick={handleDelete}>삭제</button>
-                </div>
-                <div className="order">
-                  <div className="minus" onClick={handleMinus}>
-                    -
-                  </div>
-                  <div className="count">{list.amount}</div>
-                  <div className="plus" onClick={handlePlus}>
-                    +
+          {cart.map(function (list, i) {
+            return (
+              <div className="list">
+                <div className="listLeft">
+                  <img src={list.thumb} />
+                  <div className="listArticle">
+                    <h3>{list.category_name}</h3>
+                    <span>
+                      {list.ml_volume}ml / {list.tags.join('')}용
+                    </span>
+                    <p>{list.price.toLocaleString()}원</p>
                   </div>
                 </div>
+
+                <div className="listRight">
+                  <div className="cancel">
+                    <button
+                      onClick={() => {
+                        fetch('data/cartData.json', {
+                          method: 'delete',
+                          body: JSON.stringify({
+                            id: list.id,
+                          }),
+                        })
+                          .then(res => res.json())
+                          .then(data => {
+                            data = 'success'
+                              ? setCart(
+                                  cart.filter(cartList => {
+                                    return cartList.id !== list.id;
+                                  })
+                                )
+                              : null;
+                          })
+                          .catch(error => alert(error));
+                      }}
+                    >
+                      삭제
+                    </button>
+                  </div>
+                  <div className="order">
+                    <div
+                      className="minus"
+                      onClick={() => {
+                        if (list.amount === 1) {
+                          alert('상품을 더 이상 줄일 수 없습니다.');
+                        }
+                        fetch('data/cartData.json', {
+                          method: 'patch',
+                          body: JSON.stringify({
+                            id: list.id,
+                            amount: list.amount - 1,
+                          }),
+                        })
+                          .then(res => res.json())
+                          .then(data => {
+                            data = 'success' ? (list.amount += 1) : null;
+                          })
+                          .catch(error => alert(error));
+                      }}
+                    >
+                      -
+                    </div>
+                    <div className="count">{list.amount}</div>
+                    <div
+                      className="plus"
+                      onClick={() => {
+                        if (list.amount === 99) {
+                          alert('제품을 더 이상 추가할수 없습니다.');
+                        }
+                        fetch('data/cartData.json', {
+                          method: 'patch',
+                          body: JSON.stringify({
+                            id: list.id,
+                            amount: list.amount + 1,
+                          }),
+                        })
+                          .then(res => res.json())
+                          .then(data => {
+                            data = 'success' ? (list.amount += 1) : null;
+                          })
+                          .catch(error => alert(error));
+                      }}
+                    >
+                      +
+                    </div>
+                  </div>
+                </div>
               </div>
+            );
+          })}
+
+          <div className="total">
+            <div className="fee">
+              <p>배송비</p>
+              <p>무료배송</p>
             </div>
-          );
-        })}
-
-        <div className="total">
-          <div className="fee">
-            <p>배송비</p>
-            <p>무료배송</p>
+            <div className="sum">
+              <p>총 결제금액</p>
+              <p>
+                {cart
+                  .reduce((total, curr) => total + curr.price * curr.amount, 0)
+                  .toLocaleString()}
+                원
+              </p>
+            </div>
           </div>
-          <div className="sum">
-            <p>총 결제금액</p>
-            <p>
-              {cart
-                .reduce((total, curr) => total + curr.price * curr.count, 0)
-                .toLocaleString()}
-              원
-            </p>
-          </div>
-        </div>
 
-        <div className="orderBtn">
-          <button>주문하기</button>
-        </div>
-      </main>
+          <div className="orderBtn">
+            <button>주문하기</button>
+          </div>
+        </main>
+      )}
     </>
   );
 }
