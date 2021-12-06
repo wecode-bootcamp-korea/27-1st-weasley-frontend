14fa918e9ad9adc8823ce5aef3a2e49ca11a2b87
yuryang Kim <flowratekim@gmail.com>
2021-12-06 13:34:05 +0900

    Modify : router, nav page

--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Main from './pages/Main/Main';
 import ProductList from './pages/ProductList/ProductList';
 import ProductDetail from './pages/ProductDetail/ProductDetail';
 import Login from './pages/Login/Login';
