b18dc58ab01794559d8d6e1b050e7a94b20e1726
seongjae0325 <90776802+seongjae0325@users.noreply.github.com>
2021-12-06 20:35:51 +0900

    [27기 황성재]ingredient페이지

--- src/pages/Ingredient/IngredientList.js
+++ src/pages/Ingredient/IngredientList.js
@@ -1,37 +1,41 @@
 import React from 'react';
 import './IngredientList.scss';
-import IngredientTitle from './IngredientTitle';
+// import IngredientTitle from './IngredientTitle';
 
 function IngredientList() {
   return (
     <>
       <h3 className="ingredientCategory">클렌징폼</h3>
-      <ul className="ingredientList">
-        <li className="ingredient">
-          <p className="type">
-            지성/복합성 피부용
-            <h4 className="name">소듐코코일알라니네이트</h4>
-          </p>
-        </li>
-        <li className="ingredient">
-          <p className="type">
-            지성/복합성 피부용
-            <h4 className="name">소듐코코일알라니네이트</h4>
-          </p>
-        </li>
-        <li className="ingredient">
-          <p className="type">
-            지성/복합성 피부용
-            <h4 className="name">소듐코코일알라니네이트</h4>
-          </p>
-        </li>
-        <li className="ingredient">
-          <p className="type">
-            지성/복합성 피부용
-            <h4 className="name">소듐코코일알라니네이트</h4>
-          </p>
-        </li>
-      </ul>
+      <button className="leftButton">⇦</button>
+      <button className="rightButton">⇨</button>
+      <div className="container">
+        <ul className="ingredientList">
+          <li className="ingredient">
+            <p className="type">
+              지성/복합성 피부용
+              <h4 className="name">소듐코코일알라니네이트</h4>
+            </p>
+          </li>
+          <li className="ingredient">
+            <p className="type">
+              지성/복합성 피부용
+              <h4 className="name">소듐코코일알라니네이트</h4>
+            </p>
+          </li>
+          <li className="ingredient">
+            <p className="type">
+              지성/복합성 피부용
+              <h4 className="name">소듐코코일알라니네이트</h4>
+            </p>
+          </li>
+          <li className="ingredient">
+            <p className="type">
+              지성/복합성 피부용
+              <h4 className="name">소듐코코일알라니네이트</h4>
+            </p>
+          </li>
+        </ul>
+      </div>
     </>
   );
 }
