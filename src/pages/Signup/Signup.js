11be862aa1f29029a49a197ebb2971833f0efc12
yuryang Kim <flowratekim@gmail.com>
2021-12-06 14:02:55 +0900

    Test : nav bar

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import USER_DATA from './UserData.js';
+import Nav from '../../components/Nav';
 import './Signup.scss';
 
 function Signup() {
