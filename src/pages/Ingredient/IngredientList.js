cad7f2c818a6bc81df3d6dc11dec0ab881ae76c1
seongjae0325 <90776802+seongjae0325@users.noreply.github.com>
2021-12-07 09:12:20 +0900

    [27기 황성재]핵심성분 페이지

--- src/pages/Ingredient/IngredientList.js
+++ src/pages/Ingredient/IngredientList.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './IngredientList.scss';
 // import IngredientTitle from './IngredientTitle';
 
 function IngredientList() {
+  const [xPos, setXpos] = useState(0);
+
+  const onClick = direction => {
+    direction === 'left' ? setXpos(x => x - 100) : setXpos(x => x + 100);
+  };
   return (
     <>
       <h3 className="ingredientCategory">클렌징폼</h3>
-      <button className="leftButton">⇦</button>
-      <button className="rightButton">⇨</button>
+      <button
+        className="leftButton ingredientBtn"
+        onClick={() => onClick('left')}
+      >
+        ⇦
+      </button>
+      <button
+        className="rightButton ingredientBtn"
+        onClick={() => onClick('right')}
+      >
+        ⇨
+      </button>
       <div className="container">
-        <ul className="ingredientList">
+        <ul
+          className="ingredientList"
+          style={{ transform: `translateX(${xPos}px)` }}
+        >
           <li className="ingredient">
             <p className="type">
               지성/복합성 피부용
