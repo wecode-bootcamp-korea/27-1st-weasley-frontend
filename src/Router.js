3be4f895580da54198a05487fd558d1114ecfdad
yuryang Kim <flowratekim@gmail.com>
2021-12-07 10:34:30 +0900

    Add: 회원 가입 기능 완성

--- src/Router.js
+++ src/Router.js
@@ -5,10 +5,12 @@ import ProductDetail from './pages/ProductDetail/ProductDetail';
 import Login from './pages/Login/Login';
 import Payment from './pages/payment/Payment';
 import Signup from './pages/Signup/Signup';
+import Nav from './components/Nav/Nav';
 
 const Router = () => {
   return (
     <BrowserRouter>
+      <Nav />
       <Routes>
         <Route path="/productlist" element={<ProductList />} />
         <Route path="/productdetails/:id" element={<ProductDetail />} />
