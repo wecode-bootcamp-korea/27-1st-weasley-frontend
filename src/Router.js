3183ffe8a4feb65991e409f8905d4c559f9a8d85
JooyoungHwnag <71796608+JooyoungHwnag@users.noreply.github.com>
2021-12-07 17:22:38 +0900

    Add: 메인페이지 완료

--- src/Router.js
+++ src/Router.js
@@ -14,7 +14,7 @@ const Router = () => {
     <BrowserRouter>
       <Nav />
       <Routes>
-        <Route path="/main" element={<Main />} />
+        <Route path="/" element={<Main />} />
         <Route path="/productlist" element={<ProductList />} />
         <Route path="/productdetails/:id" element={<ProductDetail />} />
         <Route path="/login" element={<Login />} />
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -5,7 +5,7 @@ import './Main.scss';
 const Footer = () => {
   return (
     <main className="mainPage">
-      <section className="mainPageSection">
+      <section className="mainTopSection">
         <h1 className="mainTitleTag">
           현명한 소비를 위한 <br />
           정직한 스킨케어
@@ -15,17 +15,27 @@ const Footer = () => {
         </h3>
         <form className="viewProductForm">
           <button type="button" className="viewNowProductButton">
-            시작하기
+            <Link to="/productlist">시작하기</Link>
           </button>
         </form>
       </section>
-      <section className="mainPageSection">
+      <section className="mainMiddleSection">
         <h1 className="mainTitleTag">더 이상 속지마세요.</h1>
         <h3 className="subTitleTag">
-          시중 브랜드들이 화장품의 효과를 과장하고 가격을 부풀려온 탓에,
-          남자들은 좋은 제품을 고르기 어려웠습니다. 오픈워크는 불합리한 시장
-          관행을 거부하고, 정직한 기준을 만들어갑니다.
+          <p>시중 브랜드들이 화장품의 효과를 과장하고 가격을 부풀려온 탓에,</p>
+          <p>남자들은 좋은 제품을 고르기 어려웠습니다.</p>
+          <p>
+            오픈워크는 불합리한 시장 관행을 거부하고, 정직한 기준을
+            만들어갑니다.
+          </p>
         </h3>
+      </section>
+      <section className="mainBottomSection">
+        <h1 className="mainTitleTag">
+          효과적인 제품.
+          <br /> 그리고 정직한 가격.
+        </h1>
+        <p className="mainElement">핵심성분 보러가기</p>
       </section>
     </main>
   );
