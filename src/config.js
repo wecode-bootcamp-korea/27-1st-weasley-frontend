3313149f1b890a6891d71713e4c13b546993feac
yuryang Kim <flowratekim@gmail.com>
2021-12-08 23:15:40 +0900

    WIP

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ const BASE_URL = 'http://3.142.147.114:8000';
 
 export const API = {
   CART: `${BASE_URL}/shops/carts`,
-  SIGNIN: `${BASE_URL}/users/signIn`,
+  SIGNIN: `${BASE_URL}/users/signin`,
   SIGNUP: `${BASE_URL}users/signup`,
 };
