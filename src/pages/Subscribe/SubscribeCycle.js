3c7a64952cb1eb3287b606eabbfb1e7beba2b752
seongjae0325 <90776802+seongjae0325@users.noreply.github.com>
2021-12-09 14:10:58 +0900

    Modify: patch요청 api수정

--- src/pages/Subscribe/SubscribeCycle.js
+++ src/pages/Subscribe/SubscribeCycle.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import { API } from '../../config';
 import './SubscribeCycle.scss';
 
 function SubscribeCycle({ setDeliveryCycle, setNextDeliveryDate }) {
   const cycle = [4, 12, 16];
 
   const handleNextShipDate = week => {
-    fetch('da', {
-      method: 'patch',
+    fetch(API.SUBSCRIBE, {
+      method: 'PATCH',
       headers: {
         Authorization:
           'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6Mn0.bHQK7d38oajQKa3Hl8nsYrqDhp9m2fmo_MWjDWMN4Zs',
