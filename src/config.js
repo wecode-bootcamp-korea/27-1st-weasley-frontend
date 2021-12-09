251842df3df37c7f1700f7b0f62d6fe32ebc76f9
JooyoungHwnag <71796608+JooyoungHwnag@users.noreply.github.com>
2021-12-09 17:16:11 +0900

    modfiy:conflict

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ const BASE_URL = 'http://3.142.147.114:8000';
 export const API = {
   CART: `${BASE_URL}/shops/carts`,
   SIGNIN: `${BASE_URL}/users/signin`,
-  // SIGNUP: `${BASE_URL}/users/signup`,
+  SIGNUP: `${BASE_URL}/users/signup`,
   SUBSCRIBE: `${BASE_URL}/shops/subscribes`,
   PRODUCT_DETAIL: `${BASE_URL}/products`,
   ORDER: `${BASE_URL}/shops/orders`,
